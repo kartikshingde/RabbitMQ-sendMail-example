ee2ed3cc48c7019a24569104f73d065b7559ca89
KartikShingde <kartikshingde12@gmail.com>
2025-12-31 23:57:09 +0530

    rabbitMQ setup,on docker as well as code,used 1producer,1consumer

---
+++ consumer.js
@@ -0,0 +1,24 @@
+const amqp=require('amqplib')
+
+async function recvMail(){
+    try {
+        
+        const connection = await amqp.connect("amqp://localhost");
+        
+        const channel = await connection.createChannel();
+        
+        await channel.assertQueue("mail_queue",{durable:false})
+
+        channel.consume("mail_queue",(message)=>{
+            if(message!==null){
+                console.log("Recv Message ",JSON.parse(message.content));
+                channel.ack(message);
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+recvMail()
---
+++ producer.js
@@ -0,0 +1,37 @@
+const amqp = require("amqplib");
+
+async function sendMail() {
+  try {
+    const connection = await amqp.connect("amqp://localhost");
+
+    const channel = await connection.createChannel();
+
+    const exchange = "mail_exchange";
+    const routingKey="send_mail";
+
+    const message={
+        to:"realtime@gmail.com",
+        from:"harish@gmail.com",
+        subject:"Thank You",
+        body:"Hello abc"
+    }
+
+    await channel.assertExchange(exchange,"direct",{durable:false});
+    await channel.assertQueue("mail_queue",{durable:false});
+
+    await channel.bindQueue("mail_queue",exchange,routingKey)
+
+    channel.publish(exchange,routingKey,Buffer.from(JSON.stringify(message)));
+
+    console.log("Mail data was sent",message)
+
+    setTimeout(()=>{
+        connection.close();
+    },500)
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+sendMail();
